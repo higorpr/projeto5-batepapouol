260eaf595d9457e3cd429f957a9d7023b3a87fd3
Higor de Faria <higorpr@gmail.com>
2022-08-24 14:27:02 -0300

    Initial Layont completed

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const msg = {name: 'Higor'};
+// const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', msg);
+// request.then(printFunction);
+
+const entry = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+entry.then(initialDisplay);
+
+function initialDisplay(response) {
+    /**
+     * This function receives the response from the initial axios.get request
+     * and populates the chat
+     */
+
+    console.log(response);
+}
