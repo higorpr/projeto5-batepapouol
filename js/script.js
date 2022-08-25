15480b2405ecd4dbade23b3117c34d8b6f627bce
Higor de Faria <higorpr@gmail.com>
2022-08-25 01:07:38 -0300

    Login page completed - HTML + CSS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+
 // const msg = {name: 'Higor'};
 // const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', msg);
 // request.then(printFunction);
 
 const entry = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
 entry.then(initialDisplay);
+
+function enterChat() {
+    
+}
 
 function initialDisplay(response) {
     /**
